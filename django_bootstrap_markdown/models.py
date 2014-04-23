a42edae24638c36771ad52aae2dc8e0241fd19e4
A.J. May <amay@northps.com>
2014-04-22 23:31:26 -0400

    fix rotation of iOS images

--- django_bootstrap_markdown/models.py
+++ django_bootstrap_markdown/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFit, SmartResize
+from imagekit.processors import Transpose, ResizeToFit, SmartResize
 
 
 class Image(models.Model):
     original = models.ImageField(upload_to='markdown/images/')
     large = ImageSpecField(
         source='original',
-        processors=[ResizeToFit(750, 400, upscale=False)],
+        processors=[Transpose(), ResizeToFit(750, 400, upscale=False)],
         options={'quality': 75}
     )
     medium = ImageSpecField(
         source='original',
-        processors=[ResizeToFit(375, 400, upscale=False)],
+        processors=[Transpose(), ResizeToFit(375, 400, upscale=False)],
         options={'quality': 75}
     )
     small = ImageSpecField(
         source='original',
-        processors=[ResizeToFit(185, 400, upscale=False)],
+        processors=[Transpose(), ResizeToFit(185, 400, upscale=False)],
         options={'quality': 75}
     )
     billboard = ImageSpecField(
         source='original',
-        processors=[SmartResize(750, 300)],
+        processors=[Transpose(), SmartResize(750, 300)],
         options={'quality': 75}
     )
     description = models.CharField(max_length=200)
