ce9f2812fa0c2281b1e0a4bd00d098d6b63ae2a9
A.J. May <amay@northps.com>
2014-05-20 10:45:43 -0400

    convert markdown to rst

--- setup.py
+++ setup.py
@@ -3,12 +3,23 @@
 import os
 from setuptools import setup, find_packages
 
+try:
+    from pypandoc import convert
 
-readme = open('README.rst').read()
+    def read_md(f):
+        try:
+            d = convert(f, 'rst')
+        except OSError:
+            print("warning: You do not have pandoc installed, could not convert README.") 
+            d = open(f, 'r').read()
+        return d
+except ImportError:
+    print("warning: pypandoc module not found, could not convert README.")
+    read_md = lambda f: open(f, 'r').read()
 
 setup(
     name="django-bootstrap-markdown",
-    version="1.5.3",
+    version="1.5.4",
     packages=find_packages(),
     author="A.J. May",
     author_email="aj7may@gmail.com",
@@ -16,7 +27,7 @@ setup(
     maintainer_email="aj7may@gmail.com",
     description="""An extension of the Django Textarea widget made for
     editing Markdown with a live preview.""",
-    long_description=readme,
+    long_description=read_md('README.md'),
     license="MIT License",
     keywords="django bootstrap markdown live preview auto scroll",
     url="http://thegoods.aj7may.com/django-bootstrap-markdown",
